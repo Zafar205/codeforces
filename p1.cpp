aa5e70f1f570812e0e44e49b8bb18bfd8206df3a
Zafar205 <mz09206@st.habib.edu.pk>
2025-02-14 10:50:30 +0500

    more changes

---
+++ p1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+void func(string s) {
+    if (s.size() <= 10) {  // Should be <= 10 (not < 10)
+        cout << s << "\n"; // Print word and move to new line
+    } else {
+        cout << s.front() << s.size() - 2 << s.back() << "\n";  // Use .front() and .back()
+    }
+}
+ 
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        string s;
+        cin >> s;
+        func(s);
+    }
+    return 0;
+}
