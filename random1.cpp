fef03dbb7aba905b0647ba742b81053bcb0d8c98
Zafar205 <mz09206@st.habib.edu.pk>
2025-03-04 22:46:51 +0500

    further editions

---
+++ random1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(int argc, char* argv[]){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    for(size_t i = 0 ; i < argc ; ++i){
+        cout << argv[i] << endl;
+    }
+
+    
+
+}
+
+
+
+
+
+
